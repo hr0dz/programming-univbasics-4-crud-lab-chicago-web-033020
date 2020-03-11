c9475931edfcc0399819b48cdc36dd86ffa3c6e5
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-11 00:59:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   catz = ["meow" , "purr" , "tabby" , "turtoise shell"]
-  catz << 
+  catz << "whiskers"
 end
 
 def add_element_to_start_of_array(array, element)
